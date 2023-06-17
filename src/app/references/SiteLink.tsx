e643e8189ecde9631539d6600cc3e4ea92299a53
박승훈 <psh1126@midasin.com>
2023-06-18 03:38:36 +0900

    style: references 페이지 스타일링 1차 및 타입 리팩토링

--- src/app/references/SiteLink.tsx
+++ src/app/references/SiteLink.tsx
@@ -2,27 +2,41 @@
 
 import { FC } from 'react';
 import { css } from '@emotion/react';
+import { SiteLinkType } from '@/app/references/data';
 
-interface SiteLinkProps {
-  href: string;
-  name: string;
-  isBlank?: boolean;
+interface SiteLinksProps {
+  links: SiteLinkType[];
 }
 
-const SiteLink: FC<SiteLinkProps> = ({ href, name, isBlank = true }) => {
-  return (
-    <li css={listStyle}>
-      <a target={isBlank ? '_blank' : ''} href={href}>
-        {name}
-      </a>
-    </li>
-  );
-};
+export const SiteLinkList: FC<SiteLinksProps> = ({ links }) => (
+  <ul>
+    {links.map(({ name, href, isBlank = true }) => (
+      <SiteLink key={name} href={href} name={name} isBlank={isBlank} />
+    ))}
+  </ul>
+);
+
+const SiteLink: FC<SiteLinkType> = ({ href, name, isBlank = true }) => (
+  <li>
+    <a css={anchorStyle} target={isBlank ? '_blank' : ''} href={href}>
+      {name}
+    </a>
+  </li>
+);
 
 export default SiteLink;
 
-const listStyle = css`
+const anchorStyle = css`
+  display: block;
+  padding: 18px;
+  background-color: #f0f0f0;
   color: black;
+  border-radius: 6px;
+  transition: background-color 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #e5e5e5;
+  }
 
   &:visited {
     color: black;
--- src/app/references/page.tsx
+++ src/app/references/page.tsx
@@ -1,48 +1,55 @@
 'use client';
 
-import SiteLink from '@/app/references/SiteLink';
+import { SiteLinkList } from '@/app/references/SiteLink';
 import { css } from '@emotion/react';
 import { NextPage } from 'next';
+import { projectSetting, studyLibrary } from '@/app/references/data';
 
 const Docs: NextPage = () => {
   return (
-    <main>
+    <main css={containerStyle}>
       <h1>참고자료</h1>
       <h2>1. 프로젝트 설정</h2>
-      <ul>
-        <SiteLink
-          href={'https://velog.io/@brgndy/Next.js-13-%EB%9D%BC%EC%9A%B0%ED%8C%85'}
-          name={'1.1. Next13 프로젝트 폴더 구조'}
-        />
-        <SiteLink href={'https://velog.io/@rmaomina/prettier-eslint-settings'} name={'1.2. Prettier, ESLint 설정'} />
-        <SiteLink
-          href={'https://flamingotiger.github.io/frontend/react/nextjs-absolute-path/'}
-          name={'1.3. Next.js에서 절대경로 alias 설정'}
-        />
-        <SiteLink
-          href={'https://velog.io/@s_sangs/CSS-in-JS-Stylelint-feat.-emotion-styled-components'}
-          name={'1.4. CSS-in-JS stylelint 설정'}
-        />
-        <SiteLink
-          href={'https://stackoverflow.com/questions/244777/can-comments-be-used-in-json'}
-          name={'1.5. JSON 주석 사용'}
-        />
-      </ul>
-      <br />
+      <SiteLinkList links={projectSetting} />
+
       <h2>2. 라이브러리 적응기</h2>
       <h3>2.1. emotion</h3>
-      <ul>
-        <SiteLink
-          href={'https://nuhends.tistory.com/125'}
-          name={'2.1.1. emotion 사용 중 css 속성 사용시 타입 에러 해결 방법'}
-        />
-        <SiteLink
-          href={'https://velog.io/@favorcho/Emotion-%EC%86%8C%EA%B0%9C-%EB%B0%8F-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0'}
-          name={'2.1.2. emotion 소개 및 사용하기'}
-        />
-      </ul>
+      <SiteLinkList links={studyLibrary} />
     </main>
   );
 };
 
 export default Docs;
+
+const containerStyle = css`
+  margin: 0 auto;
+  width: 100%;
+  min-width: 800px;
+  max-width: 2000px;
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    > li:last-child {
+      margin-bottom: 50px;
+    }
+  }
+
+  h1 {
+    margin-bottom: 30px;
+  }
+
+  h2 {
+    margin: 30px 0 20px 0;
+    font-weight: 700;
+    font-size: 28px;
+  }
+
+  h3 {
+    margin: 20px 0 10px 0;
+    font-weight: 500;
+    font-size: 20px;
+  }
+`;
